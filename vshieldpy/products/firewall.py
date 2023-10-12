cb7a5aae0928dfdc3139c894b097551ae1760392
Tayfun Bocek <tayfunbocek@live.ca>
2023-10-12 23:08:02 +0300

    Add firewall dataclasses.

---
+++ vshieldpy/products/firewall.py
@@ -0,0 +1,95 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass(slots=True)
+class Attack:
+    """Old attack containing information about the attack.
+
+    Attributes:
+        identifier: Identifier of the attack.
+        average_per_second: Average requests received per second.
+        peak: Peak requests per second.
+        percent_blocked: Percentage of the blocked requests.
+        status: True if online and False if offline.
+        date: Start date of the attack.
+        end_date: End date of the attack.
+    """
+
+    identifier: int
+    average_per_second: int
+    peak: int
+    percent_blocked: int
+    status: bool
+    date: datetime
+    end_date: datetime
+
+
+@dataclass(slots=True)
+class Subdomain:
+    """Subdomain and its current status.
+
+    Attributes:
+        identifier: Identifier of the Subdomain.
+        subdomain: Subdomain URI value.
+        uam: If the UAM is enabled results in True, else value is False.
+        rate_limit: TODO
+        backend: TODO
+        date: Date of when the subdomain was created/added.
+    """
+
+    identifier: int
+    subdomain: str
+    uam: bool
+    rate_limit: int
+    backend: str
+    date: datetime
+
+
+@dataclass(slots=True)
+class Subdomains:
+    """Subdomains object prividing practival methods for lookups."""
+
+    subdomains: tuple[Subdomain]
+
+    def __iter__(self):
+        """Yields contained `~Subdomain`'s."""
+        yield from self.subdomains
+
+    def __getitem__(self, subdomain_uri: str) -> Subdomain | None:
+        """Get `~Subdomain` via the subdomain."""
+        subdomain_uri = subdomain_uri.rstrip()
+        for subdomain in self.subdomains:
+            if subdomain.subdomain == subdomain_uri:
+                return subdomain
+        return None
+
+    def subdomains_as_str(self) -> list[str]:
+        """Iterate over subdomains as strings."""
+        return [subdomain.subdomain for subdomain in self.subdomains]
+
+
+# TODO add practical methods for Subdomains (__iter__, from_domain, etc.).
+
+
+@dataclass(slots=True)
+class Firewall:
+    """Firewall object containing firewall stats.
+
+    Contains subdomain's along with old and new attack statuses.
+
+    Attributes:
+        is_under_attack: True if currently under attack and False if it isnt.
+        allowed_req_per_sec: Requests that passed the firewall.
+            This is the total number of accepted requests containing good
+             (and possibly bad) requests.
+        blocked_req_per_sec: Blocked requests per second.
+        subdomains: Subdomains container for all subdomains.
+        history: Old attacks that were performed on the domain.
+    """
+
+    is_under_attack: bool
+    allowed_req_per_sec: int
+    blocked_req_per_sec: int
+    subdomains: Subdomains
+    history: tuple[Attack]
