0afb47d3102ccee447740e80b051001c814162af
Tayfun Bocek <tayfunbocek@live.ca>
2023-10-12 23:11:59 +0300

    Better docstrings for services.

--- vshieldpy/products/service.py
+++ vshieldpy/products/service.py
@@ -6,7 +6,14 @@ from datetime import datetime
 
 @dataclass(slots=True)
 class DedicatedServer:
-    """Dedicated server object with ease of use methods."""
+    """Dedicated server object with ease of use methods.
+
+    Attributes:
+        identifier: Identifier of the dedicated server.
+        ip: IP address of the dedicated server.
+        status: If server is running value is True if not False.
+        expiration: Date of expiration of the server.
+    """
 
     identifier: int
     ip: str
@@ -16,7 +23,14 @@ class DedicatedServer:
 
 @dataclass(slots=True)
 class Hosting:
-    """Hosting server object with ease of use methods."""
+    """Hosting object containing related information.
+    
+    Attributes:
+        identifier: Identifier of the hosting.
+        domain: Domain assigned to the hosting.
+        status: If running value is True if not value is False.
+        expiration: Date of when the hosting expires.
+    """
 
     identifier: int
     domain: str
