bed7380ef8c867156158d9dc7a416c052f671e73
Tayfun Bocek <tayfunbocek@live.ca>
2023-11-07 02:33:01 +0300

    Add Windows Server 2019 as an option. (Only needed for VDS orders at the moment)

--- vshieldpy/api_defs/operating_systems.py
+++ vshieldpy/api_defs/operating_systems.py
@@ -7,6 +7,7 @@ class OperatingSystems(Enum):
     """Operating systems that can be installed on server's and dedicated server's."""
 
     WindowsServer22 = "winserv22"
+    WindowsServer19 = "winserv19"
     Debian10 = "debian10"
     Ubuntu20 = "ubuntu20"
     Centos9 = "centos9"
