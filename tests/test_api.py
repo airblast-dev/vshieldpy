e274fcd4564c335febb9689a264c70a1f8200f1f
Tayfun Bocek <tayfunbocek@live.ca>
2024-03-05 17:43:18 +0300

    Add TODO notes for testing.

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,7 +13,6 @@ os.environ["VS_API_KEY"] = "1"
 import vshieldpy  # noqa: E402
 
 
-
 # FIX: This almost always works, however a proper way to assure the server has started should be implemented.
 @pytest.fixture(scope="session", autouse=True)
 def test_server():
@@ -43,3 +42,8 @@ client = vshieldpy.Client()
 
 # TODO: Implement `system` test calls. 
 # This would probably better to do once a standard way to provide locations is impleneted.
+
+# TODO: Implement testing for return type methods, and validate class properties, fields, and method based API calls.
+# Validating method based API calls is low priority since it simply passes arguments to the underlying client.
+# Meaning there is a very big chance type checkers will catch any mistakes, since they only contain little to no logic.
+# However things like server statistic's related methods, or properties can contain complex logic, and should be tested.
