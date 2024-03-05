2eec08be515bf726ab99672e8117f00ec6cba17c
Tayfun Bocek <tayfunbocek@live.ca>
2024-03-06 02:22:32 +0300

    Update UAMStatus to include the automatic setting.

--- vshieldpy/api_defs/uam_status.py
+++ vshieldpy/api_defs/uam_status.py
@@ -1,6 +1,11 @@
+"""UAM Status related items."""
+
 from enum import Enum
 
+
 class UAMStatus(Enum):
-    Disabled = 0,
-    Automatic = 1,
+    """UAM status currently selected on the panel."""
+
+    Disabled = (0,)
+    Automatic = (1,)
     Forced = 2
