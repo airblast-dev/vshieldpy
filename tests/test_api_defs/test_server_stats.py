0d23ad6db425256060f524d4d3ec127ad59bb8ca
Tayfun Bocek <tayfunbocek@live.ca>
2024-03-09 03:04:14 +0300

    more testing

---
+++ tests/test_api_defs/test_server_stats.py
@@ -0,0 +1,24 @@
+import pytest
+
+from vshieldpy.client import Client
+
+pytestmark = pytest.mark.anyio
+
+client = Client()
+
+
+async def test_server_stats():
+    server_stats = await client.fetch_server_stats(1)
+    assert server_stats.cpu_avg_last_1hr == 1
+    assert server_stats.cpu_avg_last_24hrs == 1
+
+    assert 4.764 > server_stats.ram_avg_last_1hr > 4.763
+    assert 4.764 > server_stats.ram_avg_last_24hrs > 4.763
+
+    download, upload = server_stats.network_avg_last_1hr
+    assert 0.104 > download > 0.10
+    assert 0.067 > upload > 0.066
+
+    download, upload = server_stats.network_avg_last_24hrs
+    assert 0.104 > download > 0.10
+    assert 0.067 > upload > 0.066
---
+++ tests/test_api_defs/test_stock_status.py
@@ -0,0 +1,29 @@
+import pytest
+
+from vshieldpy.api_defs.locations import Locations
+from vshieldpy.api_defs.plans import Plans
+from vshieldpy.client import Client
+
+pytestmark = pytest.mark.anyio
+
+
+client = Client()
+
+
+async def test_stock_status_check_stock():
+    stock_status = await client.fetch_plans()
+    assert stock_status.check_stock(Plans.VDS_1, Locations.Netherlands)
+    assert stock_status.check_stock(Plans.VDS_2, Locations.Netherlands)
+    assert not stock_status.check_stock(Plans.VDS_3, Locations.Netherlands)
+
+
+async def test_stock_status_get_locations():
+    stock_status = await client.fetch_plans()
+    assert (Locations.Netherlands, Locations.US) == stock_status.get_locations(
+        Plans.VDS_3
+    )
+    assert (
+        Locations.Europe,
+        Locations.Singapore,
+        Locations.US,
+    ) == stock_status.get_locations(Plans.VDS_PRO_BRONZE)
