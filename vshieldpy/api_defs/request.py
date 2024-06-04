ebfb1629ffb346b6317f44e04f0870aebce161eb
Tayfun Bocek <tayfunbocek@live.ca>
2024-06-04 15:01:31 +0300

    fix: Update domain name of vshield API.

--- vshieldpy/api_defs/request.py
+++ vshieldpy/api_defs/request.py
@@ -5,7 +5,7 @@ from os import getenv
 from httpx import URL
 
 # Env variable should only be used if vShields API path is changed, or for testing purposes.
-VS_API_URL = getenv("VS_API_URL") or "https://api.vshield.pro"
+VS_API_URL = getenv("VS_API_URL") or "https://api.vshield.com"
 
 
 def _url_factory(url: str) -> URL:
